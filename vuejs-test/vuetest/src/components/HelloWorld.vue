7fce06d0e5fd7cb52fd06a5e4df733b9e008859b
Yann Mingant <yann.mingant@gmail.com>
2022-10-25 21:53:32 +0200

    update

---
+++ vuejs-test/vuetest/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const name = ref('yann')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vuejs-test/vuetest/tests/HelloWorld.test.js
@@ -0,0 +1,9 @@
+import { HelloWorld }from '"@/components/HelloWorld.vue"';
+
+describe("Mes tests vite" , ()=>{
+  it('test my component' , ()=>{
+    
+    const wrapper = mount(HelloWorld)
+
+  })
+})
